83d0505520bd96222feb5f3167bafcda0c458335
zxx-457 <zxx457@outlook.com>
2022-12-29 18:20:44 +0800

    datatypes

--- src/koans/19_datatypes.clj
+++ src/koans/19_datatypes.clj
@@ -18,30 +18,33 @@
 (deftype Razzie [category]
   Award
   (present [this recipient]
-    __))
+    (print (str "You're really the "
+                (.category this) ", "
+                recipient "... sorry."))))
+
 
 (meditations
-  "Holding records is meaningful only when the record is worthy of you"
-  (= __ (.prize (Nobel. "peace")))
+ "Holding records is meaningful only when the record is worthy of you"
+ (= "peace" (.prize (Nobel. "peace")))
 
-  "Types are quite similar"
-  (= __ (.prize (Pulitzer. "literature")))
+ "Types are quite similar"
+ (= "literature" (.prize (Pulitzer. "literature")))
 
-  "Records may be treated like maps"
-  (= __ (:prize (Nobel. "physics")))
+ "Records may be treated like maps"
+ (= "physics" (:prize (Nobel. "physics")))
 
-  "While types may not"
-  (= __ (:prize (Pulitzer. "poetry")))
+ "While types may not"
+ (= nil (:prize (Pulitzer. "poetry")))
 
-  "Further study reveals why"
-  (= __
-     (map map? [(Nobel. "chemistry")
-                (Pulitzer. "music")]))
+ "Further study reveals why"
+ (= [true false]
+    (map map? [(Nobel. "chemistry")
+               (Pulitzer. "music")]))
 
-  "Either sort of datatype can define methods in a protocol"
-  (= __
-     (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
+ "Either sort of datatype can define methods in a protocol"
+ (= "Congratulations on your Best Picture Oscar, Evil Alien Conquerors!"
+    (with-out-str (present (Oscar. "Best Picture") "Evil Alien Conquerors")))
 
-  "Surely we can implement our own by now"
-  (= "You're really the Worst Picture, Final Destination 5... sorry."
-     (with-out-str (present (Razzie. "Worst Picture") "Final Destination 5"))))
+ "Surely we can implement our own by now"
+ (= "You're really the Worst Picture, Final Destination 5... sorry."
+    (with-out-str (present (Razzie. "Worst Picture") "Final Destination 5"))))
